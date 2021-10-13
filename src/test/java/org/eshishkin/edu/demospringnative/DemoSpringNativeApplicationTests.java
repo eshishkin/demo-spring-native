752c4b4bffd6fa6240f59cbc6b55a9c62c2024e1
Eugene Shishkin <shishkin.john@gmail.com>
2021-10-13 19:39:12 +0400

    #1: skeleton app from spring boot initializer
    Signed-off-by: Eugene Shishkin <shishkin.john@gmail.com>

---
+++ src/test/java/org/eshishkin/edu/demospringnative/DemoSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eshishkin.edu.demospringnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
